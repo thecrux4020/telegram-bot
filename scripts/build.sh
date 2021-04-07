7298968dc1c5c60fc58f3601c5c2020697f21428
Leandro <leandro.mantovani@evertecinc.com>
2021-04-06 21:46:01 -0300

    fix some build bugs

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,5 +3,6 @@
 rm -rf ./scripts/build
 pip install --target ./scripts/package -r requirements.txt
 mkdir ./scripts/build
-zip -r ./scripts/build/deploy.zip app/ main.py ./scripts/package/
+zip -r ./scripts/build/deploy.zip app/ main.py settings.ini
+cd ./scripts/package && zip -rg ../build/deploy.zip * && cd ../../
 rm -rf ./scripts/package
