d1327b2d67d7612f2f8ebe4c337dbc157958bdb6
Leandro <leandro.mantovani@evertecinc.com>
2021-04-06 21:44:40 -0300

    put some common functions in helpers

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,5 +1,18 @@
-
+import logging
+import configparser
 
 class objectview(object):
     def __init__(self, d):
         self.__dict__ = d
+
+def setup_logging():
+    """ Basic logging setup """
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+    return logging
+    
+def get_config(config_file):
+    """ Read configuration file, and returns a configparser object """
+    conf = configparser.ConfigParser()
+    conf.read(config_file)
+    return conf
